b2d7ce58e4879e0a622450eff0a6cd5e717e7ea1
Namithact <namithact@hotmail.com>
2025-06-05 12:39:02 +0400

    Update route.js

--- my-blog-app/src/app/api/dbcheck/route.js
+++ my-blog-app/src/app/api/dbcheck/route.js
@@ -1,14 +1,12 @@
-// src/app/api/dbcheck/route.js
-
+// src/app/api/test-post/route.js
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    await prisma.$connect();
-    return new Response("✅ Connected to DB!");
-  } catch (error) {
-    console.error("❌ DB connection failed:", error);
-    return new Response("❌ DB connection failed", { status: 500 });
+    const posts = await prisma.post.findMany();
+    return Response.json(posts);
+  } catch (err) {
+    return new Response("❌ Query failed: " + err.message, { status: 500 });
   }
 }
